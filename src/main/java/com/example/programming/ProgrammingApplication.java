b51a85433d73f035193ec9822fe4d3bf70f2a6cf
hcjw <78733543+hcjw@users.noreply.github.com>
2022-03-04 21:14:45 +0900

    Display_login.html

--- src/main/java/com/example/programming/ProgrammingApplication.java
+++ src/main/java/com/example/programming/ProgrammingApplication.java
@@ -2,10 +2,18 @@ package com.example.programming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class ProgrammingApplication {
 
+    @RequestMapping("/")
+    String home() {
+      return "login";
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(ProgrammingApplication.class, args);
 	}
